8b9494755c92645eb1df43d8c6c11530160db773
DARKXSIDE78 <nk1010057@gmail.com>
2025-01-24 13:37:19 +0530

    Update loader.jsx

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -4,8 +4,8 @@ import './Loader.css'; // update with the correct path to your image
 const Loader = () => {
   return (
     <div className="loader-container z-9999">
-      <img src="" alt="GenAnime" className="loader-image" />
-      <p className="loader-text ">Created By <span className='font-bold'>GenAnime</span></p>
+      <img src="" alt="    GenAnime" className="loader-image" />
+      <p className="loader-text ">Created By <span className='font-bold'>DARKXSIDE78</span></p>
       <div className="loader-bar">
         <div className="loader-progress"></div>
       </div>
