558b742e7f3d60295a874b5c7adc68d734c26b3c
DARKXSIDE78 <nk1010057@gmail.com>
2025-01-24 11:29:13 +0530

    Update loader.jsx

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import './Loader.css';
-import logo from '../../images/icon.png'; // update with the correct path to your image
+import './Loader.css'; // update with the correct path to your image
 
 const Loader = () => {
   return (
     <div className="loader-container z-9999">
-      <img src={logo} alt="Logo" className="loader-image" />
+      <img src="" alt="GenAnime" className="loader-image" />
       <p className="loader-text ">Created By <span className='font-bold'>GenAnime</span></p>
       <div className="loader-bar">
         <div className="loader-progress"></div>
